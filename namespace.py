297372116075162b22515efa542f3a58ccb3f26b
tapinear <nevekon@yandex.ru>
2024-07-30 02:22:47 +0700

    Задание для проверки

---
+++ namespace.py
@@ -0,0 +1,9 @@
+
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function() # Вызов функции внутри функции test_function
+
+# inner_function()  # NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
+# print(inner_function()) # NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
+print(test_function())
